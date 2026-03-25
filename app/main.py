31368213637b455b2d1799363c56eeb5280c65f4
AndrewGluss <bangluss@gmail.com>
2026-03-25 19:30:35 +0300

    add endpoints for orders

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 
-from app.routers import cart, categories, products, reviews,users
+from app.routers import cart, categories, orders, products, reviews, users
 
 
 
@@ -16,6 +16,7 @@ app.include_router(products.router)
 app.include_router(users.router)
 app.include_router(reviews.router)
 app.include_router(cart.router)
+app.include_router(orders.router)
 
 # Корневой эндпоинт для проверки
 @app.get("/")
